f7d19077e5675d88d227b3804c9f8424dc070355
tobiasluna1 <tobiasluna1@mi.unc.edu.ar>
2025-08-21 18:00:31 -0300

    Update main.cpp

--- semana3/obj/src/main.cpp
+++ semana3/obj/src/main.cpp
@@ -5,5 +5,9 @@ using namespace std;
 
 int main() {
     // Instancia un objeto de la clase Sensor
+    sensor* s1 = new sensor();
+    //s1->nombre = "Temp Cocina";
+    //cout << s1->nombre << endl;
+    s1->setNombre("Temp Cocina") 
     return 0;
 }
