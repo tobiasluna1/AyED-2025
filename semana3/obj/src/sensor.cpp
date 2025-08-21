05caf591884f1d805d048495fa904d0517be99f2
tobiasluna1 <tobiasluna1@mi.unc.edu.ar>
2025-08-21 17:55:53 -0300

    Update sensor.cpp

--- semana3/obj/src/sensor.cpp
+++ semana3/obj/src/sensor.cpp
@@ -2,3 +2,9 @@
 #include <iostream>
 #include <fstream>
 #include <iomanip>
+
+std::string Sensor(std::string nombre, float valor, std::string unidad) {
+    this->nombre = nombre;    
+    this->valor = valor;
+    this->unidad = unidad;
+}
