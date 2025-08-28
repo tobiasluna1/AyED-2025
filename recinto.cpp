086ed624c3d672a76d2780af789ccba69086d648
tobiasluna1 <tobiasluna1@mi.unc.edu.ar>
2025-08-28 18:00:00 -0300

    Update recinto.cpp

--- recinto.cpp
+++ recinto.cpp
@@ -30,16 +30,41 @@ void Recinto::setNombre(std::string nombreNuevo){
 
 float Recinto::_suma(Sensor** v, int n, std::string unidad){
     //TODO
+    if (n == this->nSensores) return 0.0;
+    else {
+        if (v[n] != nullptr && v[n]->getUnidad() == unidad)
+            return _suma(v, n+1, unidad) + v[n]->getValor();
+        else
+            return _suma(v, m+1, unidad);
+    }
 }
 
 int Recinto::_maximo(Sensor** v, int n, std::string unidad, int mejorPos){
-    //TODO
+    //TODO    
+    if(n==this->nSensores) return mejorPos;
+    else{
+        if(v[n]!=nullptr && v[n]->getnidad()==unidad){
+            if(mejorPos == -1 || v[mejorPos]->getValor() <= v[n]->getValor()){
+                mejorPos = n;
+            }
+        }
+    }
 }
 
 int Recinto::_buscar(Sensor** v, int n, const std::string& key){
     //TODO
+    
 }
 
 Sensor* Recinto::_getSensor(Sensor** v, int n, int posBuscada){
-    //TODO
+    //TODO   
+    if (n==nSensores) {
+        return v[n];
+    } else {
+        if (n==posBuscada) {
+            return v[n];
+        } else {
+            return _getSensor(v,n+1, posBuscada);
+        }
+    }
 }
